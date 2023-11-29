affe4e6b123a766173b135dad98c4ff135caf5f1
wildaadia <wildaadia97@gmail.com>
2023-11-29 19:47:31 +0700

    cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-<<<<<<< HEAD
-=======
-    defaultCommandTimeout: 5500
->>>>>>> 2f68e9109f1e104d2476aca3a2a949f2e790f66e
+    baseUrl: 'http://localhost:1234'
+  
   },
 });
